035156b9a4ea6e17d387bc84169b8d20e49393a4
pcalderon <pedro.calderon@edocsaude.com>
2017-07-17 15:08:38 -0300

    Adjust queue path name

--- src/print.js
+++ src/print.js
@@ -1,17 +1,18 @@
 const electron = require('electron')
 const moment = require('moment')
+const R = require('ramda')
 
 electron.ipcRenderer.on('ticket', (e, ticket) => {
   document.getElementById('ticket-number').innerHTML = ticket.ticket
-  document.getElementById('ticket-queue').innerHTML = ticket.queue
+  document.getElementById('ticket-queue').innerHTML = R.path(['queue', 'name'], ticket)
   document.getElementById('ticket-preferred').innerHTML =
     ticket.preferred ? 'Preferencial' : 'Normal'
 
   const date = ticket.createdAt || moment().valueOf()
-  document.getElementById('ticket-date-time').innerHTML = 
+  document.getElementById('ticket-date-time').innerHTML =
     moment(date).format('DD/MM/YYYY - HH:mm')
 
   // TODO: get from ticket
-  document.getElementById('ticket-organization').innerHTML = 
+  document.getElementById('ticket-organization').innerHTML =
     'Previna taipas'
 })
